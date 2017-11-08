ecb6b03a1dcc58230dca3efe6485cee9db2a2933
Lisa Koperski <lisa_koperski_junk@yahoo.com>
2017-11-08 22:35:26 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
-# code your #position_taken? method here!
+# code your #position_taken? method here!def position_taken?(board,index)
+# require "pry"
+
+def position_taken?(board,index)
+  # binding.pry
+    if board[index] == "X" || board[index] == "O"
+       return true
+    else
+       return false
+    end
+end
